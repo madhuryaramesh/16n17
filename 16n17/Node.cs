73059f17070a68eb0dbb09cae9e07ab0ffc9e3e9
madhurya <madhuryar017@gmail.com>
2022-01-10 00:18:04 +0530

    Adding UC2

---
+++ 16n17/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16n17
+{
+    class Node
+    {
+        public Node LeftNode { get; set; }
+
+        public Node RightNode { get; set; }
+
+        public int Data { get; set; }
+
+    }
+}
--- 16n17/Program.cs
+++ 16n17/Program.cs
@@ -4,45 +4,16 @@ namespace _16n17
 {  
     class Program
     {
-        public static bool checkEquality(String[] s1, String[] s2)
+        static void Main(string[] args)
         {
-            if (s1 == s2)
-            {
-                return true;
-            }
-            if (s1 == null || s2 == null)
-            {
-                return false;
-            }
-            int n = s1.Length;
-            if (n != s2.Length)
-            {
-                return false;
-            }
-            for (int i = 0; i < n; i++)
-            {
-                if (!s1[i].Equals(s2[i]))
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        class program
-        {
-            static void Main(String[] args)
-            {
-                String[] s1 = { "A", "B", "C" };
-                String[] s2 = { "A", "B", "C" };
-                if (checkEquality(s1, s2))
-                {
-                    Console.WriteLine("Both arrays are equal");
-                }
-                else
-                {
-                    Console.WriteLine("Both arrays are not equal");
-                }
-            }
+            BinaryTree binaryTree = new BinaryTree();
+
+            binaryTree.Add(1);
+            binaryTree.Add(2);
+            binaryTree.Add(7);
+            binaryTree.Add(3);
+            binaryTree.Add(0);
+            binaryTree.TraversePreorder(binaryTree.Root);
         }
     }
 }
