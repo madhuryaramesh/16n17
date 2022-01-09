1cfe8daeb382d27ffcaed0afba7c77f315c06b86
madhurya <madhuryar017@gmail.com>
2022-01-10 00:06:35 +0530

    Second Commit UC1

--- 16n17/Program.cs
+++ 16n17/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 
-namespace _c
-{
+namespace _16n17
+{  
     class Program
     {
-        static void Main(string[] args)
+        public static bool checkEquality(String[] s1, String[] s2)
         {
-           Console.WriteLine("WELCOME TO 16 AND 17 ASSIGNMENT!");
+            if (s1 == s2)
+            {
+                return true;
+            }
+            if (s1 == null || s2 == null)
+            {
+                return false;
+            }
+            int n = s1.Length;
+            if (n != s2.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (!s1[i].Equals(s2[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        class program
+        {
+            static void Main(String[] args)
+            {
+                String[] s1 = { "A", "B", "C" };
+                String[] s2 = { "A", "B", "C" };
+                if (checkEquality(s1, s2))
+                {
+                    Console.WriteLine("Both arrays are equal");
+                }
+                else
+                {
+                    Console.WriteLine("Both arrays are not equal");
+                }
+            }
         }
     }
 }
